96d036daa427a863d3c676f73f4a606639478454
coel <yhu847@connect.hkust-gz.edu.cn>
2024-11-18 13:57:53 +0800

    feat: 修改deploy脚本

--- cuda/11.8/deploy.sh
+++ cuda/11.8/deploy.sh
@@ -3,7 +3,7 @@
 docker run -d --name huyu-cuda11.8 \
 --restart unless-stopped \
 --gpus all \
--p 127.0.0.1:22000:22/tcp \
+-p 0.0.0.0:22000:22/tcp \
 -v /home/hdd/huyu/shared:/root/hdd \
 -v /home/huyu/shared/:/root/ssd \
 huyu/cuda:11.8-ubuntu22.04
