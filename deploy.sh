37cca19a0961cab988ace83239b9412c46ce9f35
coel <yhu847@connect.hkust-gz.edu.cn>
2024-12-28 21:13:09 +0800

    feat: 添加修改uid脚本

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,42 @@
 #!/usr/bin/env bash
 
+USER_NAME="developer"
+
 # modify here
 CONTAINER_NAME="huyu-cuda11.8"
-HOSTNAME="cuda_11_8"
+HOSTNAME="3090x8_cu11"
 IMAGE_NAME="huyu/cuda:11.8-ubuntu22.04"
+HDD_ROOT=/home/hdd/huyu/container/
+SSD_ROOT=/home/huyu/container/
+SSH_PORT=22000
 
 function deploy() {
+    # create data directory
+    mkdir -p "$SSD_ROOT/data"
+    mkdir -p "$HDD_ROOT/data"
+    mkdir -p "$HDD_ROOT/anaconda3/envs"
+    mkdir -p "$HDD_ROOT/anaconda3/pkgs"
+    mkdir -p "$HDD_ROOT/vscode-server"
+    mkdir -p "$HDD_ROOT/cache/huggingface"
+    mkdir -p "$HDD_ROOT/cache/pip"
+    
+    
+    USER_HOME="/home/$USER_NAME"
+    
     docker run -d --name="$CONTAINER_NAME" \
     --restart=unless-stopped \
     --gpus=all \
     --privileged \
     --shm-size=2g \
     --hostname="$HOSTNAME" \
-    -p "0.0.0.0:22000:22/tcp" \
-    -v "/home/hdd/huyu/container/data:/home/developer/hdd" \
-    -v "/home/ssd/huyu/container/data:/home/developer/ssd" \
-    -v "/home/hdd/huyu/container/anaconda3/envs:/home/developer/anaconda3/envs" \
-    -v "/home/hdd/huyu/container/anaconda3/pkgs:/home/developer/anaconda3/pkgs" \
-    -v "/home/hdd/huyu/container/vscode-server:/home/developer/.vscode-server" \
-    -v "/home/hdd/huyu/container/cache/huggingface:/home/developer/.cache/huggingface" \
-    -v "/home/hdd/huyu/container/cache/pip:/home/developer/.cache/pip" \
+    -p "0.0.0.0:$SSH_PORT:22/tcp" \
+    -v "$SSD_ROOT/data:$USER_HOME/ssd" \
+    -v "$HDD_ROOT/data:$USER_HOME/hdd" \
+    -v "$HDD_ROOT/anaconda3/envs:$USER_HOME/anaconda3/envs" \
+    -v "$HDD_ROOT/anaconda3/pkgs:$USER_HOME/anaconda3/pkgs" \
+    -v "$HDD_ROOT/vscode-server:$USER_HOME/.vscode-server" \
+    -v "$HDD_ROOT/cache/huggingface:$USER_HOME/.cache/huggingface" \
+    -v "$HDD_ROOT/cache/pip:$USER_HOME/.cache/pip" \
     "$IMAGE_NAME"
 }
 
